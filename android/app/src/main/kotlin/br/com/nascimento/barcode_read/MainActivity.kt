6bf715812d1f273d9f0e27bb61ac67e61e273352
nascimento92 <nascimento92@users.noreply.github.com>
2021-01-21 21:47:36 -0300

    First Commit

---
+++ android/app/src/main/kotlin/br/com/nascimento/barcode_read/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.nascimento.barcode_read
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+
+void main(){
+  runApp(MaterialApp(
+    title: "Leitor de Código de barras",
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  String _data = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          FlatButton(
+            child: Text("Scan"),
+            onPressed: ()=> _scan(),
+            color: Colors.red,
+          ),
+          Container(
+            alignment: Alignment.center,
+            child: Text(_data),
+          )
+        ],
+      ),
+    );
+  }
+
+  _scan() async{
+      await FlutterBarcodeScanner.scanBarcode("#000000", "Cancel", true, ScanMode.BARCODE).then((value) => setState(()=> _data = value));
+  }
+}
