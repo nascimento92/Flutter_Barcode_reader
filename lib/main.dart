c162230a093fe42a49a02bbaabc1271dbafe651b
nascimento92 <nascimento92@users.noreply.github.com>
2021-01-25 20:50:00 -0300

    news

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
-void main(){
+void main() {
   runApp(MaterialApp(
     title: "Leitor de Código de barras",
     home: Home(),
@@ -19,15 +19,24 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey,
+      ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          FlatButton(
-            child: Text("Scan"),
-            onPressed: ()=> _scan(),
-            color: Colors.green,
+          Padding(
+              padding: EdgeInsets.only(top: 50.0),
+              child: Icon(Icons.qr_code_scanner, size: 150.0)),
+          Padding(
+            padding: EdgeInsets.fromLTRB(80.0, 10.0, 80.0, 5.0),
+            child:  FlatButton(
+              child: Text("Bipar", style: TextStyle(color: Colors.white)),
+              onPressed: () => _scan(),
+              color: Colors.grey,
+            )
           ),
+
           Container(
             alignment: Alignment.center,
             child: Text(_data),
@@ -37,7 +46,15 @@ class _HomeState extends State<Home> {
     );
   }
 
-  _scan() async{
-      await FlutterBarcodeScanner.scanBarcode("#000000", "Cancel", true, ScanMode.BARCODE).then((value) => setState(()=> _data = value));
+  _scan() async {
+    String codbar = await FlutterBarcodeScanner.scanBarcode(
+        "#8B0000", "Cancel", true, ScanMode.BARCODE);
+    setState(() {
+      if (codbar == "-1") {
+        _data = "Código de Barras inválido!";
+      } else {
+        _data = codbar;
+      }
+    });
   }
 }
